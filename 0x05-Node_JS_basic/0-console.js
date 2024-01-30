5940d14fc6c7fecce1342805ce5d056c968f02b9
Stanhope Chinonso <stanhope.c.arthur@gmail.com>
2024-01-30 10:32:20 +0100

    npm install eslint --save-dev

---
+++ 0x05-Node_JS_basic/0-console.js
@@ -0,0 +1,10 @@
+/**
+ * Displays a message on the STDOUT.
+ * @param {String} msg The message to display.
+ * @author Stanhope Chinonso
+ */
+const displayMessage = (msg) => {
+    console.log(msg);
+  };
+  
+  module.exports = displayMessage;

47bd10f4647346986dd87e2b7a8b5903d19d2230
Stanhope Chinonso <stanhope.c.arthur@gmail.com>
2024-03-10 17:49:53 +0100

    0-console.js Hot fix

--- 0x05-Node_JS_basic/0-console.js
+++ 0x05-Node_JS_basic/0-console.js
@@ -4,7 +4,7 @@
  * @author Stanhope Chinonso
  */
 const displayMessage = (msg) => {
-    console.log(msg);
-  };
-  
-  module.exports = displayMessage;
+  console.log(msg);
+};
+
+module.exports = displayMessage;
